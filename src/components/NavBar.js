0283a6fd7a0d4cf13e2412c649bc90ffb969c19d
Rownok <rownok2001@gmail.com>
2023-02-16 20:05:14 +0600

    added signup page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import {FaHatWizard} from 'react-icons/fa'
 import {BiHomeCircle} from 'react-icons/bi'
 import {TbNotification} from 'react-icons/tb'
@@ -8,17 +14,34 @@ import rownok from '../assets/rownok.png'
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState } from "react";
 import PostCreate from './PostCreate'
+import SignUp from './SignUp';
+import Content from './Content';
+import SuggestedContent from './SuggestedContent';
 const NavBar = () => {
     let [isOpen, setIsOpen] = useState(false);
+    let [isSignUp, setSignUp] = useState(false);
+    function closeSign() {
+      setSignUp(false);
+    
+    }
+  
+    function openSign() {
+      setSignUp(true);
+     
+    }
     function closeModal() {
         setIsOpen(false);
+      
       }
     
       function openModal() {
         setIsOpen(true);
+       
       }
   return (
-    <div className='relative'>
+   
+      
+      <div className='relative'>
      <div className='flex flex-col justify-start mt-10 h-[650px] sticky left-0 top-0 items-start gap-7'>
         <FaHatWizard className='text-3xl'/>
         <div className="flex items-center gap-2 cursor-pointer">
@@ -72,8 +95,8 @@ const NavBar = () => {
               </div>
             </Dialog>
           </Transition>
-          
-        <div className="flex justify-between items-center absolute bottom-0 cursor-pointer">
+    
+          <button  className="flex justify-between items-center absolute bottom-0 cursor-pointer" onClick={openSign}>
             <div className="flex gap-1">
                <img className='w-12' src={rownok} alt="" />
                <div className="flex flex-col">
@@ -81,9 +104,48 @@ const NavBar = () => {
                 <p className='text-xs text-gray-500'>@rownok</p>
                </div>
             </div>
-        </div>
+        </button>
+        <Transition appear show={isSignUp} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeSign}>
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+              </Transition.Child>
+
+              <div className="fixed inset-0 overflow-y-auto">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                  >
+                    <Dialog.Panel className="w-full max-w-md flex justify-center items-center transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+                    <SignUp/>
+                   
+                    </Dialog.Panel>
+                  </Transition.Child>
+                </div>
+              </div>
+            </Dialog>
+          </Transition>
+          
+        
     </div>
     </div>
+     
+   
+   
    
   )
 }
