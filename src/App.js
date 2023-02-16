9560580361266483cd3592ab038d7a8a9aa5ec93
Rownok <rownok2001@gmail.com>
2023-02-16 09:29:35 +0600

    created file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+  <NavBar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='container mx-auto'>
+hjhgj
+    </div>
+  )
+}
+
+export default NavBar
